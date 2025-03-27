8fb8bac75825b3611a9601b495265d9a8eb3847e
Kawish Zia <kawishzia2826@gmail.com>
2025-03-27 19:36:23 +0500

    add err functionaltiy if user make to give wrong id

--- ExpressJS CRUD with MongoDb/controller/contact.controller.js
+++ ExpressJS CRUD with MongoDb/controller/contact.controller.js
@@ -1,55 +1,130 @@
 
 import Contact from "../models/contact_module.js"; 
 
+import mongoose from 'mongoose';
+
+export const getContact = async (req, res) => {
 
 
-export const getContact  = async(req, res) => {
-  const contact  = await Contact.findById(req.params.id);
-  // res.json(contact);
 
 
-res.render("show-contact.ejs",{contact});
-}
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+  
+    res.render("show-contact.ejs", { contact });
+  } catch (error) {
+    res.render('404',{message:error.message})
+  }
+};
 
 export const addContact = (req, res) => {
+
   res.render("add-contact.ejs");
 }
 
 export const addContacts = async (req, res) => {
-  // const contact =await Contact.insertOne({
-  //   first_name: req.body.first_name,
-  //   last_name: req.body.last_name,
-  //   email: req.body.email,
-  //   phone: req.body.phone,
-  //   address : req.body.address
-  // })
-  // {user_name,last_name,email,phone,address}=req.body
+
   await Contact.create(req.body);
-  // res.send(req.body)
 
   res.redirect("/");
 }
 
 export const updateContact =  async (req, res) => {
-  const contact = await Contact.findById(req.params.id);
-  res.render("update-contact.ejs",{contact});
+ try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+  
+    res.render("update-contact.ejs", { contact });
+  } catch (error) {
+    res.render('404',{message:error.message})
+  };
+  
 }
 
 export const  updateContacts = async(req, res) => {
-  await Contact.findByIdAndUpdate(req.params.id, req.body);
-  res.redirect("/");
+   try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+
+    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!contact) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+  
+     res.redirect("/");
+  } catch (error) {
+    res.render('404',{message:error.message})
+  }
+
+ 
 }
 
 export const deleteContact = async(req, res) => {
-  await Contact.findByIdAndDelete(req.params.id);
-  res.redirect("/");
+  
+ 
+   try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+
+    const contact = await Contact.findByIdAndDelete(req.params.id);
+
+    if (!contact) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+  
+     res.redirect("/");
+  } catch (error) {
+    res.render('404',{message:error.message})
+  }
 }
 
 
 export const allContact = async(req, res) => {
-  const contact  =await Contact.find();
-// res.json(contact);
 
+  
+  
 
-  res.render("all-post.ejs",{contact});
+ try {
+   
+  
+
+    const contact  =await Contact.find();
+
+    if (!contact) {
+      return res.render("404.ejs", { message: "Page not found" });
+    }
+  
+  
+    res.render("all-post.ejs",{contact});
+  } catch (error) {
+    res.render('404',{message:error.message})
+  }
+  
 }
