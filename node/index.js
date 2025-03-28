beb1efea442b613c017b7759275d88fa3b2f909a
Kawish Zia <kawishzia2826@gmail.com>
2025-03-28 21:55:37 +0500

    make a task or mza aya

--- node/index.js
+++ node/index.js
@@ -1,11 +1,45 @@
-const fs = require('fs');
+const express = require("express");
+const app = express();
+const fs = require('fs')
+const path = require("path");
+// middleware
+app.use(express.json())
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+const folderName = __dirname + "/files";
+try {
+  if (fs.existsSync(folderName)) {
+    // fs.mkdirSync(folderName);
+    var filee  = fs.readdirSync(folderName);
+
+    console.log(filee);
+    
+  }
+} catch (err) {
+  console.error(err);
+}
 
 
 
-fs.writeFile('text.txt',"this is simple textt",(err)=>{
-console.log(err);
 
+app.get("/", (req, res) => 
+{
+
+res.render('index',{filee});
 })
 
+app.post('/create',(req,res)=>{
+  // res.json(req.body.desc)
+  fs.writeFile(__dirname + "/files/"+req.body.title.split(" ").join(""),req.body.desc,(err)=>{
+console.log(err);
 
 
+  }
+
+)
+
+res.redirect('/');
+})
+app.listen(3000, () => console.log(` apppp listening on port port! 3000 /n http://localhost:port`));
