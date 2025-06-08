d8dc588fc5088fcf650d94bc00fc1fc7ee2d2a71
Kawish Zia <kawishzia2826@gmail.com>
2025-06-08 21:31:25 +0500

    update

---
+++ DataTable/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const User = require('./model/users.model');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+// Connect MongoDB
+mongoose.connect('mongodb+srv://kawish:kawishzia1234@cluster0.nwdrgwl.mongodb.net/users_demo')
+.then(() => console.log('Database Connected!'));
+
+// Get all users
+app.get('/api/users', async (req, res) => {
+
+  const users = await User.find();
+  res.json({ data: users });
+});
+
+app.listen(3000, () => console.log('Server started'));
