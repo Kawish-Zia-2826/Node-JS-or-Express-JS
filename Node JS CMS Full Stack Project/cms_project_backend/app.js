493a386c7fbf0f5f474c72355d293892ee87b7ea
Kawish Zia <kawishzia2826@gmail.com>
2025-06-26 18:34:53 +0500

    day one:

---
+++ Node JS CMS Full Stack Project/cms_project_backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const session = require('express-session');
+const flash = require('connect-flash');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts'); 
+dotenv.config();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
+
+// Viw Engine
+app.set('view engine', 'ejs');
+
+
+
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('Connected to DB')
+);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
